6a58ac98b077f963cb8971ca148f7203c54a12ee
khady2204 <khady.kandji3@unchk.edu.sn>
2025-06-29 21:30:06 +0000

    S

---
+++ src/app/services/user-auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthService {
+
+  private readonly IdKey = 'Id';
+  private readonly tokenKey = 'jwtToken';
+
+  setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  setId(Id: string): void {
+    localStorage.setItem(this.IdKey, Id);
+  }
+
+  getId(): string | null {
+    return localStorage.getItem(this.IdKey);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+}
