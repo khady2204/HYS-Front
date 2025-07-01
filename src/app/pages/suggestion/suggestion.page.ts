e998c3bf980213a3a074329dbd29044793d8cdf4
AhmadouBambaSow <ahmadoubambasow900@gmail.com>
2025-07-01 12:05:44 +0000

    Mise à jour de la page suggestion

--- src/app/pages/suggestion/suggestion.page.ts
+++ src/app/pages/suggestion/suggestion.page.ts
@@ -9,40 +9,20 @@ import { HeaderSearchComponent } from '../../shared/header-search/header-search.
   templateUrl: './suggestion.page.html',
   styleUrls: ['./suggestion.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, HeaderSearchComponent]
+  imports: [IonContent, CommonModule, FormsModule]
 })
 export class SuggestionPage implements OnInit {
 
-  stories = [
-    {name: 'Christie B.', img: 'assets/img/myLOve/story/story01.png' },
-    {name: 'Mrs. Rodolfo', img: 'assets/img/myLOve/story/story02.png' },
-    {name: 'Eloise Legrand', img: 'assets/img/myLOve/story/story03.png' },
-    {name: 'Ahmed S.', img: 'assets/img/myLOve/story/story02.png' }
-  ];
+  
 
   suggestions = [
-    {
-      role: 'Beautician',
-      name: 'Miss Zachary Will',
-      description: 'Doloribus saepe aut necessitat qui non qui.',
-      note: '4.9',
-      bg: 'assets/img/myLOve/story/story03.png'
-    },
-    {
-      role: 'Beautician',
-      name: 'Miss Zachary Will',
-      description: 'Doloribus saepe aut necessitat qui non qui.',
-      note: '4.9',
-      bg: 'assets/img/myLOve/story/story03.png'
-    },
-    {
-      role: 'Beautician',
-      name: 'Miss Zachary Will',
-      description: 'Doloribus saepe aut necessitat qui non qui.',
-      note: '4.9',
-      bg: 'assets/img/myLOve/story/story03.png'
-    }
+    { name: 'Fatou Ly', compatibility: '93', distance: '5 km', photo: 'assets/img/myLOve/suggestion/user1.png' },
+    { name: 'Safy Sankara', compatibility: '90', distance: '2 km', photo: 'assets/img/myLOve/suggestion/user2.png' },
+    { name: 'Aicha Ndiaye', compatibility: '86', distance: '13 km', photo: 'assets/img/myLOve/suggestion/user3.png' },
+    { name: 'Hawa Sow', compatibility: '78', distance: '8 km', photo: 'assets/img/myLOve/suggestion/user4.png' },
+    { name: 'Salma Fall', compatibility: '60', distance: '4 km', photo: 'assets/img/myLOve/suggestion/user5.png' }
   ]
+selectedTab: any;
 
   constructor() { }
 
