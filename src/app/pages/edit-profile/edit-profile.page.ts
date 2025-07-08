45766963dd656395e99178a934052621755a6355
AhmadouBambaSow <ahmadoubambasow900@gmail.com>
2025-07-08 09:29:45 +0000

    MAJ

--- src/app/pages/edit-profile/edit-profile.page.ts
+++ src/app/pages/edit-profile/edit-profile.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonSelectOption, IonSelect } from '@ionic/angular/standalone';
+import { FloatingMenuComponent } from 'src/app/components/floating-menu/floating-menu.component';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.page.html',
   styleUrls: ['./edit-profile.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule]
+  imports: [IonContent, CommonModule, FormsModule, FloatingMenuComponent]
 })
 export class EditProfilePage implements OnInit {
 
