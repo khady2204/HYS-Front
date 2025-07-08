086eae4bf0bfed5f01b9d04d61e27b7b34c6677b
AhmadouBambaSow <ahmadoubambasow900@gmail.com>
2025-07-08 10:20:35 +0000

    mise à jour de la navigation

--- src/app/shared/header-search/header-search.component.ts
+++ src/app/shared/header-search/header-search.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-search',
   templateUrl: './header-search.component.html',
   styleUrls: ['./header-search.component.scss'],
+  imports: [RouterLink],
   standalone: true
 })
 export class HeaderSearchComponent  implements OnInit {
