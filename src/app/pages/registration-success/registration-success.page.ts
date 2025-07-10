f9722a7ad04095e79130ac0053d981a99587bfab
khady2204 <khady.kandji3@unchk.edu.sn>
2025-07-10 14:39:13 +0000

    Réinitialisation

---
+++ src/app/pages/registration-success/registration-success.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-registration-success',
+  templateUrl: './registration-success.page.html',
+  styleUrls: ['./registration-success.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule]
+})
+export class RegistrationSuccessPage implements OnInit {
+
+  constructor(
+    private router:Router) { }
+
+  ngOnInit() {
+  }
+
+}
