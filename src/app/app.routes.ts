46eda4134270fb44d14e49254c20fb4b662c84d7
Abdoulaye <segnanelaye@gmail.com>
2025-07-21 21:35:53 +0000

    Remplacer le contenu de dev par celui de khady

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  
   {
     path: 'register',
     loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
@@ -39,64 +40,10 @@ export const routes: Routes = [
     path: 'new-password',
     loadComponent: () => import('./pages/new-password/new-password.page').then( m => m.NewPasswordPage)
   },
-
-  {
-    path: 'validationsms',
-    loadComponent: () => import('./pages/validationsms/validationsms.page').then( m => m.ValidationsmsPage)
-  },
-  {
-    path: 'registration-success',
-    loadComponent: () => import('./pages/registration-success/registration-success.page').then( m => m.RegistrationSuccessPage)
-  },
-  {
-    path: 'confirmation',
-    loadComponent: () => import('./pages/confirmation/confirmation.page').then( m => m.ConfirmationPage)
-  },
-  {
-    path: 'new-password',
-    loadComponent: () => import('./pages/new-password/new-password.page').then( m => m.NewPasswordPage)
-  },
-
-
-  {
-    path: 'suggestion',
-    loadComponent: () => import('./pages/suggestion/suggestion.page').then( m => m.SuggestionPage)
-  },
-
   {
     path: 'otp-verification',
     loadComponent: () => import('./pages/otp-verification/otp-verification.page').then( m => m.OtpVerificationPage)
   },
-  {
-    path: 'construction',
-    loadComponent: () => import('./pages/construction/construction.page').then( m => m.ConstructionPage)
-  },
-  {
-    path: 'accueil',
-    loadComponent: () => import('./pages/accueil/accueil.page').then( m => m.AccueilPage)
-  },
-  {
-    path: 'discussions',
-    loadComponent: () => import('./pages/discussions/discussions.page').then( m => m.DiscussionsPage)
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
-  },
-  {
-    path: 'chat',
-    loadComponent: () => import('./pages/chat/chat.page').then( m => m.ChatPage)
-  },
-  {
-    path: 'edit-profile',
-    loadComponent: () => import('./pages/edit-profile/edit-profile.page').then( m => m.EditProfilePage)
-  },
-  {
-    path: 'report-partner',
-    loadComponent: () => import('./pages/report-partner/report-partner.page').then( m => m.ReportPartnerPage)
-  },
-
-
 
 
 ];
