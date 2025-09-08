597d3130a231cbc94670c9cf087e4270d10435aa
khady2204 <khady.kandji3@unchk.edu.sn>
2025-09-08 20:04:41 +0000

    Tests et Correction partie authentification

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,23 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private ApiUrl = 'http://192.168.1.23:8081/api/auth';
+  //private ApiUrl = 'http://192.168.1.15:8081';
+
+  //private ApiUrl = 'http://10.0.2.2:8081/api/auth';
+
+  private ApiUrl = environment.apiBase ;
 
 
   constructor(private http: HttpClient) {}
 
   register(data: any): Observable<any>{
-    return this.http.post(`${this.ApiUrl}/register`, data, { responseType: 'text' });
+    return this.http.post(`${this.ApiUrl}/api/auth/register`, data, { responseType: 'text' });
   }
 
   login(credentials: { email: string; phone: string; password: string }): Observable<any> {
-    return this.http.post<any>(`${this.ApiUrl}/login`, credentials);
+    return this.http.post<any>(`${this.ApiUrl}/api/auth/login`, credentials);
   }
 
   /**
@@ -27,7 +32,7 @@ export class AuthService {
    * @returns Observable avec l'OTP (en test)
    */
   sendOtp(data: any): Observable<any> {
-    return this.http.post(`${this.ApiUrl}/send-otp`, data);
+    return this.http.post(`${this.ApiUrl}/api/auth/send-otp`, data);
   }
 
   /**
@@ -36,28 +41,28 @@ export class AuthService {
    * @returns Observable avec le token JWT
    */
   verifyOtp(data: any): Observable<any> {
-    return this.http.post(`${this.ApiUrl}/verify-otp`, data);
+    return this.http.post(`${this.ApiUrl}/api/auth/verify-otp`, data);
   }
 
   //Envoie un OTP pour réinitialisation de mot de passe
   // Étape 1 : Envoi OTP
   requestReset(data: any): Observable<any> {
-    return this.http.post(`${this.ApiUrl}/reset/request`, data, { responseType: 'text' });
+    return this.http.post(`${this.ApiUrl}/api/auth/reset/request`, data, { responseType: 'text' });
   }
 
   // Étape 2 : Vérification OTP
   verifyOtpForReset(data:{ phone: string; otp: string } ): Observable<any> {
-    return this.http.post(`${this.ApiUrl}/reset/verify-otp`, data, { responseType: 'text' });
+    return this.http.post(`${this.ApiUrl}/api/auth/reset/verify-otp`, data, { responseType: 'text' });
   }
 
   // Étape 3 : Changement de mot de passe
   confirmReset(data: any): Observable<any> {
-    return this.http.post(`${this.ApiUrl}/reset/password`, data,{ responseType: 'text' } );
+    return this.http.post(`${this.ApiUrl}/api/auth/reset/password`, data,{ responseType: 'text' } );
   }
 
   // logout
   logout(): Observable<any> {
-    return this.http.post(`${this.ApiUrl}/logout`, {}).pipe(
+    return this.http.post(`${this.ApiUrl}/api/auth/logout`, {}).pipe(
       tap(() => {
         // Supprimer le token localement
         localStorage.removeItem('jwtToken');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true ,
+  apiBase: 'http://10.0.2.2:8081', // change selon le cas ci-dessus
+  withCredentials: true 
 };
