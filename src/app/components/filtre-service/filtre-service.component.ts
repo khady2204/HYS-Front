8605c5e2707447b0dea71dfcedfab0ef0984f236
projet-operationnel <projetoperationnel>
2025-09-12 18:41:17 +0000

    delete scss add css

--- src/app/components/filtre-service/filtre-service.component.ts
+++ src/app/components/filtre-service/filtre-service.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filtre-service',
   templateUrl: './filtre-service.component.html',
-  styleUrls: ['./filtre-service.component.scss'],
+  styleUrls: ['./filtre-service.component.css'],
 })
 export class FiltreServiceComponent  implements OnInit {
 
