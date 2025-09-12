8075d3f196926f498588093dc8434e21d61d18ff
AhmadouBambaSow <ahmadoubambasow900@gmail.com>
2025-09-12 17:12:22 +0000

    Code améliorer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // Pour le développement web (navigateur) : localhost
   // Pour l'émulateur Android : 10.0.2.2
   // Pour l'appareil physique : 192.168.1.64 (votre IP locale)
-  apiBase: 'http://localhost:8081',
+  apiBase: 'http://192.168.54.47:8081',
   withCredentials: true            // si tu utilises cookies/session
 };
 
