a3a9a8fbcc23b42efd29ea757f5aa2e1a6e7720c
AhmadouBambaSow <ahmadoubambasow900@gmail.com>
2025-10-01 17:32:59 +0000

    Mode sombre

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true ,
-  //apiBase: 'http://api.hysinternational.com:8080', // change selon le cas ci-dessus
+  apiBase: 'http://192.168.54.47:8081', // change selon le cas ci-dessus
   withCredentials: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   production: false,
   // Pour le développement web (navigateur) : localhost
   // Pour l'émulateur Android : 10.0.2.2
-  //apiBase: 'http://localhost:8081',
+  apiBase: 'http://localhost:8081',
   // Pour l'appareil physique : 192.168.1.64 (votre IP locale)
-  apiBase: 'http://192.168.54.47:8081',
+  //apiBase: 'http://192.168.54.47:8081',
   withCredentials: true            // si tu utilises cookies/session
 };
 
